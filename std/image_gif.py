2996a08b94c0c1df586a4aaab9ff2092581274d4
Philippe Antoine <contact@catenacyber.fr>
2022-06-24 10:57:44 +0200

    std: special patch for image/gif

---
+++ std/image_gif.py
@@ -0,0 +1,20 @@
+#python
+import sys
+
+# special patch as gif.Decode needs to be checked by gif.DecodeConfig for arbitrary allocations
+patch='''			arg1 := bytes.NewReader(a.Decode.R)
+			cfg, err := gif.DecodeConfig(arg1)
+			if err != nil {
+				return 0
+			}
+			if cfg.Width * cfg.Height > 1024*1024 {
+				continue
+			}'''
+
+f = open(sys.argv[1])
+patched = False
+for l in f.readlines():
+    print(l, end="")
+    if "case *NgoloFuzzOne_Decode:" in l and not patched:
+        print(patch)
+        patched = True
