a86cc584185d4c8e496ca32186395691260c88cc
Philippe Antoine <contact@catenacyber.fr>
2022-08-26 10:36:59 +0200

    std: math.big Int.Exp patch

--- std/math_big.py
+++ std/math_big.py
@@ -4,9 +4,17 @@ import sys
 # special patch as math/big.Float.Sqrt may panic with ErrNan
 patch='''			case string, big.ErrNaN:'''
 
+# avoid too big timeout exponentiation
+patchExp='''                       if arg1.BitLen() + arg2.BitLen() > 1024 && arg3.BitLen() <= 1 {
+                                continue
+                        }'''
+
 f = open(sys.argv[1])
 for l in f.readlines():
     if "case string:" in l:
         print(patch)
+    elif "r0 := arg0.Exp(arg1, arg2, arg3)" in l:
+        print(patchExp)
+        print(l, end="")
     else:
         print(l, end="")
