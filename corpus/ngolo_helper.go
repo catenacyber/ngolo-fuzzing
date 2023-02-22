95ad2ba84c44e04df99b42f8eb6df084a9c79c08
Philippe Antoine <contact@catenacyber.fr>
2023-02-22 14:55:47 +0100

    fixup

--- corpus/ngolo_helper.go
+++ corpus/ngolo_helper.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"crypto/sha1"
-	"fmt"
+	"encoding/hex"
 	"os"
 	"path/filepath"
+
+	"google.golang.org/protobuf/proto"
 )
 
 func NgoloCorpusMarshal(item interface{}) {
 	ngolo_list := &NgoloFuzzOne{Item: item.(isNgoloFuzzOne_Item)}
 	ngolo_fuzz := NgoloFuzzList{List: []*NgoloFuzzOne{ngolo_list}}
 	data, _ := proto.Marshal(ngolo_fuzz)
-	fname := filepath.Join(os.Getenv("FUZZ_NG_CORPUS_DIR"), string(sha1.Sum(data)))
+	fname := filepath.Join(os.Getenv("FUZZ_NG_CORPUS_DIR"), string(hex.EncodeToString(sha1.Sum(data))))
 	os.WriteFile(fname, data, 0644)
 }
