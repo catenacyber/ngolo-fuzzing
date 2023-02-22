1c04626f6f4bc2d43c850d71cee0f3ad3e01914c
Philippe Antoine <contact@catenacyber.fr>
2023-02-22 14:42:01 +0100

    fixup

--- corpus/ngolo_helper.go
+++ corpus/ngolo_helper.go
@@ -11,6 +11,6 @@ func NgoloCorpusMarshal(item interface{}) {
 	ngolo_list := &NgoloFuzzOne{Item: item.(isNgoloFuzzOne_Item)}
 	ngolo_fuzz := NgoloFuzzList{List: []*NgoloFuzzOne{ngolo_list}}
 	data, _ := proto.Marshal(ngolo_fuzz)
-	fname := filepath.Join("corpus", string(sha1.Sum(data)))
+	fname := filepath.Join(os.Getenv("FUZZ_NG_CORPUS_DIR"), string(sha1.Sum(data)))
 	os.WriteFile(fname, data, 0644)
 }
